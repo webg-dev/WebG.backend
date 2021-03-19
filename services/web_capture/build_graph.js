f75d655d6a546d9f08765c17e1224dfed3d150b6
lukasmyth96 <luka@nate.tech>
2021-03-19 16:43:08 +0000

    refactor build_graph and add _wait_for_page_load func

--- services/web_capture/build_graph.js
+++ services/web_capture/build_graph.js
@@ -1,23 +1,18 @@
-window.buildGraph = function buildGraph() {
+function buildGraph() {
+    /* Returns Graph object containing nodes and edges of HTML DOM tree. */
+
     const nodes = [];
     const edges = [];
 
-    let allElements = document.querySelectorAll('*');
-    for (const [id, element] of allElements.entries()) {
-        element.setAttribute("webg_id", id.toString());
+    const  allElements = document.querySelectorAll('*');
 
+    for (const [id, element] of allElements.entries()) {
+
+        element.setAttribute("webg_id", id.toString());
         let label = element.tagName;
         let coordinates = element.getBoundingClientRect();
-        let centerX = coordinates.left + (coordinates.width / 2);
-        let centerY = coordinates.top + (coordinates.height / 2);
-        // note elementFromPoint will return null for points outside of the
-        // current viewport. This will be fixed later by using a large viewport
-        // in headless mode.
-        let elementAtCentroid = document.elementFromPoint(centerX, centerY);
-        let visible = element === elementAtCentroid;
-
-        const attributesArray = [...element.attributes];
-        const attributes = attributesArray.reduce((map, obj) => (map[obj.name] = obj.value, map), {});
+        let visible = isElementVisible(element, coordinates);
+        let attributes = getAttributesMap(element);
 
         nodes.push(
             {
@@ -29,11 +24,39 @@ window.buildGraph = function buildGraph() {
             }
         )
 
+        if (element.parentElement !== null) {
+            edges.push(
+                {
+                    from: element.parentElement.getAttribute("webg_id"),
+                    to: id
+                }
+            )
+        }
+
     }
 
     console.log(`Finished building graph with ${allElements.length} nodes`);
 
-    return {nodes}
+    return { nodes, edges }
 }
+
+function isElementVisible(element, elementCoordinates){
+    /* Returns true if element is visible to user else false.
+
+    Note that elements outside the current viewport will be marked as not visible.
+    This should be resolved by having a long viewport.
+    * */
+    const centerX = elementCoordinates.left + (elementCoordinates.width / 2);
+    const centerY = elementCoordinates.top + (elementCoordinates.height / 2);
+    const elementAtCentroid = document.elementFromPoint(centerX, centerY);
+    return element === elementAtCentroid;
+}
+
+function getAttributesMap(element) {
+    /* Returns Map containing name-value attribute pairs of element.*/
+    const attributesArray = [...element.attributes];
+    return attributesArray.reduce((map, obj) => (map[obj.name] = obj.value, map), {});
+}
+
 
 window.graph = buildGraph();
--- services/web_capture/webdriver.py
+++ services/web_capture/webdriver.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from time import time, sleep
+from typing import Optional
 
 import geckodriver_autoinstaller
 from selenium import webdriver
@@ -6,25 +8,40 @@ from selenium import webdriver
 from utils.io_funcs import read_text_file
 
 
-class WebDriver():
+class WebDriver:
 
     def __init__(self):
         geckodriver_autoinstaller.install()
         self.driver = webdriver.Firefox()
 
-    def load_page(self, url: str):
+    def load_page(self, url: str, timeout: Optional[float] = 10) -> None:
         self.driver.get(url)
+        self._wait_for_page_load(timeout)
 
-    def get_html_dom(self):
+    def _wait_for_page_load(self, timeout: float) -> None:
+        """Waits for page to fully load by checking each second
+        whether any new nodes have been added to the DOM."""
+        start_time = time()
+        previous_num_elements = 0
+        while time() - start_time < timeout:
+            num_elements = len(self.driver.find_elements_by_css_selector('*'))
+            if num_elements > previous_num_elements:
+                previous_num_elements = num_elements
+                sleep(1)
+            else:
+                break
+
+
+    def get_html_dom(self) -> str:
         return self.driver.page_source
 
-    def get_screenshot(self):
+    def get_screenshot(self) -> str:
         return self.driver.get_screenshot_as_base64()
 
-    def get_graph(self):
-        build_graph_script_path = Path(__file__).resolve().parent / 'build_graph.js'
-        build_graph_script = read_text_file(str(build_graph_script_path))
-        self.driver.execute_script(build_graph_script)
+    def get_graph(self) -> dict:
+        script_path = Path(__file__).resolve().parent / 'build_graph.js'
+        script = read_text_file(str(script_path))
+        self.driver.execute_script(script)
         graph = self.driver.execute_script(f'return graph;')
         return graph
 
@@ -32,6 +49,9 @@ class WebDriver():
 if __name__ == '__main__':
 
     _webdriver = WebDriver()
-    _webdriver.load_page('https://www.allbirds.co.uk/products/mens-tree-dashers')
+    _webdriver.load_page('https://amazon.co.uk')
     _graph = _webdriver.get_graph()
-    print(_graph)
+    print(len(_graph['nodes']))
+    sleep(5)
+    _graph = _webdriver.get_graph()
+    print(len(_graph['nodes']))
