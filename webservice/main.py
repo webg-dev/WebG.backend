fd796caaad3cca28b22b2e2765470207089c8bb4
lukasmyth96 <luka@nate.tech>
2021-04-23 13:41:49 +0100

    CORS

--- webservice/main.py
+++ webservice/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from webservice.resources import api_router
 
 app = FastAPI()
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
