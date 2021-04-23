761f134d0e9f81fda9aa48bb8c694b9c01eacb93
lukasmyth96 <luka@nate.tech>
2021-04-23 13:41:39 +0100

    support for local file urls

--- webservice/resources/web_page/endpoint.py
+++ webservice/resources/web_page/endpoint.py
@@ -1,3 +1,6 @@
+import os
+from urllib.parse import urlparse
+
 from fastapi import APIRouter
 
 from models import WebPage
@@ -8,7 +11,11 @@ router = APIRouter()
 
 @router.get('/webPage', response_model=WebPage)
 def get_web_page(url: str) -> WebPage:
+    if os.path.isfile(url):
+        url = 'file://' + url
+    else:
+        url = urlparse(url, 'https').geturl()
     webdriver = WebDriver(use_virtual_display=True)
     web_page = webdriver.get_web_page(url)
-    webdriver.close()
+    webdriver.quit()
     return web_page
