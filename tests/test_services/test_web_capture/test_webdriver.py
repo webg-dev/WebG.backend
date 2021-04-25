5824fd3dd0558df605e7e6ab8d63a17230fd8e77
lukasmyth96 <luka@nate.tech>
2021-04-25 21:17:19 +0100

    fixed test

--- tests/test_services/test_web_capture/test_webdriver.py
+++ tests/test_services/test_web_capture/test_webdriver.py
@@ -3,14 +3,13 @@ import pytest
 from services.web_capture.web_driver import WebDriver, WebPage
 
 
-@pytest.mark.parametrize('use_virtual_display', [True, False])
-def test_web_driver(use_virtual_display, url, expected_web_page_dict):
+def test_web_driver(url, expected_web_page_dict):
     """Tests that WebPage object can be captured correctly
     for a very small, fixed page both with and without the use
     of a virtual display."""
-    webdriver = WebDriver(use_virtual_display=use_virtual_display)
+    webdriver = WebDriver(use_virtual_display=True)
     web_page = webdriver.get_web_page(url)
-    webdriver.close()
+    webdriver.quit()
 
     assert isinstance(web_page, WebPage)
     assert web_page.dict() == expected_web_page_dict
