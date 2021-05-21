55266c0210ce3ff41ddb50cb27410672c803851c
lukasmyth96 <luka@nate.tech>
2021-05-21 14:13:41 +0100

    update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,19 +2,13 @@ from pathlib import Path
 
 import pytest
 
-from utils.io_funcs import read_json_file
+
+@pytest.fixture()
+def test_page_1_file_path() -> Path:
+    return Path(__file__).resolve().parent / 'mock_data/html/test_page_1.html'
 
 
 @pytest.fixture()
-def url():
-    test_html_path = Path(__file__).resolve().parent / 'mock_data/test_page_1.html'
-    url = f'file://{test_html_path}'
+def test_page_1_url(test_page_1_file_path):
+    url = f'file://{test_page_1_file_path}'
     return url
-
-
-@pytest.fixture()
-def expected_web_page_dict(url: str) -> dict:
-    expected_web_page_path = Path(__file__).resolve().parent / 'mock_data/expected_web_page_1.json'
-    expected_web_page_dict = read_json_file(expected_web_page_path)
-    expected_web_page_dict['url'] = str(url)  # must override this as path be different for each machine.
-    return expected_web_page_dict
--- tests/test_services/test_web_capture/test_webdriver.py
+++ tests/test_services/test_web_capture/test_webdriver.py
@@ -1,18 +1,29 @@
+from pathlib import Path
 from uuid import UUID
 
 import pytest
 
 from services.web_capture.web_driver import WebDriver, WebPage
+from utils.io_funcs import read_json_file
 
 
-def test_returns_expected_web_page_object(url, expected_web_page_dict):
+@pytest.fixture()
+def expected_web_page_dict_for_test_page_1(test_page_1_url) -> dict:
+    expected_web_page_path = Path(__file__).resolve().parent / 'expected_web_page_1.json'
+    expected_web_page_dict = read_json_file(expected_web_page_path)
+    expected_web_page_dict['url'] = str(test_page_1_url)  # must override this as path be different for each machine.
+    return expected_web_page_dict
+
+
+
+def test_returns_expected_web_page_object(test_page_1_url, expected_web_page_dict_for_test_page_1):
     """Tests that WebPage object can be captured correctly
     for a very small, fixed page both with and without the use
     of a virtual display."""
     webdriver = WebDriver(use_virtual_display=True)
-    web_page = webdriver.get_web_page(url)
+    web_page = webdriver.get_web_page(test_page_1_url)
     webdriver.quit()
 
     assert isinstance(web_page, WebPage)
     assert isinstance(web_page.id, UUID)
-    assert web_page.dict(exclude={'id'}) == expected_web_page_dict
+    assert web_page.dict(exclude={'id'}) == expected_web_page_dict_for_test_page_1
