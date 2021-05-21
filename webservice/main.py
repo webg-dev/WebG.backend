4f5153792abc335f65c9ebd3c6044dc81f05ab25
lukasmyth96 <luka@nate.tech>
2021-05-21 11:26:07 +0100

    attach database to FastAPI app instance

--- webservice/main.py
+++ webservice/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from services.data_storage import database
 from webservice.resources import api_router
 
 app = FastAPI()
+
 app.include_router(api_router)
+
+app.state.db = database
 
 app.add_middleware(
     CORSMiddleware,
