7bda6f5a11c20551bc09c3cbfef0287810fc341f
lukasmyth96 <luka@nate.tech>
2021-05-22 15:51:01 +0100

    add webpage fixture in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import pytest
 
+from models.web_page import WebPage
+
 
 @pytest.fixture()
 def test_page_1_file_path() -> Path:
@@ -12,3 +14,49 @@ def test_page_1_file_path() -> Path:
 def test_page_1_url(test_page_1_file_path):
     url = f'file://{test_page_1_file_path}'
     return url
+
+
+@pytest.fixture()
+def web_page() -> WebPage:
+    web_page_dict = {
+        "id": "56cd47c1-8597-4835-9958-2610eea4db6a",
+        "url": "https://test.com",
+        "viewportWidth": 1,
+        "viewportHeight": 1,
+        "html": "html",
+        "screenshot": "base64 screenshot",
+        "graph": {
+            "nodes": [
+                {
+                    "id": 0,
+                    "label": "html",
+                    "attributes": {
+                        "class": "css class name"
+                    },
+                    "coordinates": {
+                        "width": 1.0,
+                        "height": 1.0,
+                        "left": 1.0,
+                        "right": 1.0,
+                        "top": 1.0,
+                        "bottom": 1.0
+                    },
+                    "isVisible": True
+                }
+            ],
+            "edges": [
+                {
+                    "from": 0,
+                    "to": 1
+                }
+            ],
+            "labels": [
+                {
+                    "nodId": 1,
+                    "className": "className"
+                }
+            ]
+        }
+    }
+
+    return WebPage(**web_page_dict)
